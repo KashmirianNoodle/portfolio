7b2c171548cfa3bc542c13574409ccf96216023b
Mir Shafeeq <xackfaizu@gmail.com>
2026-03-29 20:09:25 +0530

    fix: added anon key

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,21 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   SUPABASE_URL,
-  VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+  SUPABASE_ANON_KEY
 );
 ;
-
-const test = async () => {
-  const { data, error } = await supabase
-    .from("visitors")
-    .select("*");
-
-  console.log('='.repeat(10))
-  console.log(data, error);
-};
-
-test();
